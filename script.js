ef98528d2aecca0ef732bb7065f4169d702d8f4f
shasharick <sharick.fernandez@cea.edu.co>
2026-04-30 08:08:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let turno = "X";
+let tablero = ["", "", "", "", "", "", "", "", ""];
 
+const celdas = document.querySelectorAll(".cell");
+
+celdas.forEach(celda => {
+    celda.addEventListener("click", () => {
+        const index = celda.dataset.index;
+
+        if (tablero[index] === "") {
+            tablero[index] = turno;
+            celda.textContent = turno;
+            turno = turno === "X" ? "O" : "X";
+        }
+    });
+});
+
+function reiniciar() {
+    tablero = ["", "", "", "", "", "", "", "", ""];
+    celdas.forEach(c => c.textContent = "");
+    turno = "X";
+}
